f0610b242af3986d1b8e57467d2e6bb054cf32e0
Diego Vistoso <diegoivg98@gmail.com>
2023-02-15 21:07:25 -0300

    import Bootstrap and Bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+/* Importing the bootstrap css file. */
+import 'bootstrap/dist/css/bootstrap.css'
+
+/* Importing the bootstrap icons. */
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// ! IMPORTANTE: los estilos propios deben ir debajo del bootstrap
 import './index.css';
 import App from './App';
+// import AppRouting from './AppRouting';
 import reportWebVitals from './reportWebVitals';
+//import AppRoutingFinal from './AppRoutingFinal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <AppRoutingFinal /> */}
   </React.StrictMode>
 );
 
