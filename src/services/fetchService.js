7f25fc25c5c37c50992c85ba435bb4eba67a06b5
Diego Vistoso <diegoivg98@gmail.com>
2023-02-22 21:57:06 -0300

    agregar funcion async login

--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -1,18 +1,36 @@
 export const getAllUser = async () => {
-    let response = await fetch('https://reqres.in/api/users')
-    console.log('Response:', response);
-    return response.json()
-}
+  let response = await fetch("https://reqres.in/api/users");
+  console.log("Response:", response);
+  return response.json();
+};
 
 export const getAllPagedUser = async (page) => {
-    let response = await fetch(`https://reqres.in/api/users?page=${page}`)
-    console.log('Response:', response);
-    return response.json()
-}
+  let response = await fetch(`https://reqres.in/api/users?page=${page}`);
+  console.log("Response:", response);
+  return response.json();
+};
 
 export const getUsersDetails = async (id) => {
-    let response = await fetch(`https://reqres.in/api/users/${id}`)
-    console.log('Response:', response);
-    return response.json()
+  let response = await fetch(`https://reqres.in/api/users/${id}`);
+  console.log("Response:", response);
+  return response.json();
+};
 
-}
+export const login = async (email, password) => {
+  let body = {
+    email:email,
+    password:password,
+  };
+
+  let response = await fetch("https://reqres.in/api/login", {
+    method: "POST",
+    // mode: "no-cors",
+    // credentials: "omit",
+    // cache: "no-cache",
+    // headers: {
+    //   "Content-type": "application/json",
+    // },
+    body: JSON.stringify(body),
+  });
+  return response.json();
+};
